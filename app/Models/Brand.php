888510215f594999d5c55e521199d3c873884b28
MusingaBrian <musingabrians@gmail.com>
2024-06-15 21:15:47 +0300

    Guard The ID Column and Add Relationship to the Brand Model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Product;
 
 class Brand extends Model
 {
     use HasFactory;
+
+    // Guarding the Id against multi assignment
+
+    protected $guarded = ['id'];
+
+    //Defining the relationship 
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
