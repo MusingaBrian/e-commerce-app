b6007b5a4befdfe03a869914d31e4635f3a6592c
MusingaBrian <musingabrians@gmail.com>
2024-06-15 21:18:34 +0300

    Guard The ID Column and Add Relationship to the Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\OrderItem;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'images' => 'array'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    public function orderItems(): BelongsTo
+    {
+        return $this->belongsTo(OrderItem::class);
+    }
 }
